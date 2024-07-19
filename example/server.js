8d945f960345250ecf940ca832985b805afd27c6
Code Icey <86058993+codeicey@users.noreply.github.com>
2024-07-19 10:01:56 +0545

    Update server.js

--- example/server.js
+++ example/server.js
@@ -8,3 +8,6 @@ const host = process.env.HOST || '0.0.0.0';
 server.listen(port, host, () => {
     console.log('Server is listening on 0.0.0.0:10000');
   });
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
