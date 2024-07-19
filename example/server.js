fd311b13b54b4cc6c5d1fd75b7a71e6945f035a4
Code Icey <86058993+codeicey@users.noreply.github.com>
2024-07-19 10:04:50 +0545

    Update server.js

--- example/server.js
+++ example/server.js
@@ -1,12 +1,12 @@
 const IRC = require('..');
 
-const server = IRC.createServer({host:"0.0.0.0"});
+const server = IRC.createServer({host:"3.75.158.163"});
 
 const port = process.env.PORT || 10000;
-const host = process.env.HOST || '0.0.0.0';
+const host = process.env.HOST || '3.75.158.163';
 
 server.listen(port, host, () => {
-    console.log('Server is listening on 0.0.0.0:10000');
+    console.log('Server is listening on 3.75.158.163:10000');
   });
 
 server.keepAliveTimeout = 120 * 1000;
