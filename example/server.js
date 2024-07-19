3aced1bb63ffc7bfa584adeb6de6ce267115d801
Code Icey <86058993+codeicey@users.noreply.github.com>
2024-07-19 09:57:00 +0545

    Update server.js

--- example/server.js
+++ example/server.js
@@ -1,7 +1,10 @@
 const IRC = require('..');
 
-const server = IRC.createServer();
+const server = IRC.createServer({host:"0.0.0.0"});
 
-server.listen(10000, '0.0.0.0', () => {
+const port = process.env.PORT || 10000;
+const host = process.env.HOST || '0.0.0.0';
+
+server.listen(port, host, () => {
     console.log('Server is listening on 0.0.0.0:10000');
   });
